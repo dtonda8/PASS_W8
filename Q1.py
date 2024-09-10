7bc82a40f2b7a6800545094317da29cc4664f26c
dtonda8 <davidbatonda7@gmail.com>
2024-09-10 21:21:55 +1000

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,5 +1,27 @@
+from data_structures.hash_table import LinearProbeTable
+
 def valid_anagram(s: str, t: str) -> bool:
-	raise NotImplementedError()
+	s_letter_cnt = LinearProbeTable()
+	t_letter_cnt = LinearProbeTable()
+
+	for ch in s:
+		if ch in s_letter_cnt:
+			s_letter_cnt[ch] += 1
+		else:
+			s_letter_cnt[ch] = 1
+
+	for ch in t:
+		if ch in t_letter_cnt:
+			t_letter_cnt[ch] += 1
+		else:
+			t_letter_cnt[ch] = 1
+		
+	for ch in s:
+		if ch not in t_letter_cnt or t_letter_cnt[ch] != s_letter_cnt[ch]:
+			return False
+	
+	return len(s) == len(t) # Ensure they have the same length (otherwise s is a subset of t)
+
 
 if __name__ == "__main__": 
 	valid_anagram("anagram", "nagaram")
--- Q2.py
+++ Q2.py
@@ -1,5 +1,9 @@
 def is_power_of_three(n: int) -> bool:
-	raise NotImplementedError()
+	if n == 1:
+		return True
+	elif n < 1:
+		return False
+	return is_power_of_three(n/3)
 
 if __name__ == "__main__":
 	pass
